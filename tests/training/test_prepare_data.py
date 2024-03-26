72c211a27dcc0ed2abcbdd3d26b8f6b4521308bd
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-03-26 22:06:45 +0000

    Adding some tests

--- tests/training/test_prepare_data.py
+++ tests/training/test_prepare_data.py
@@ -59,3 +59,71 @@ class Test_TrainingDataLoader_fetch_data(unittest.TestCase):
         self.assertEqual(subheadings, ["22222222", "33333333", "12345678", "11111111"])
         self.assertEqual(texts, [0, 1, 2, 0])
         self.assertEqual(labels, [3, 1, 2, 3])
+
+    def test_that_earlier_sources_override_later_ones(self):
+        first_data_source = StaticDataSource(
+            [("Description 1", "1234567890"), ("Description 2", "1111111111")],
+            creates_codes=True,
+            authoritative=True,
+        )
+
+        second_data_source = StaticDataSource(
+            [("Description 2", "2222222222"), ("Description 3", "3333333333")],
+            creates_codes=True,
+            authoritative=True,
+        )
+
+        (unique_texts, subheadings, texts, labels) = training_data_loader.fetch_data(
+            [first_data_source, second_data_source]
+        )
+
+        self.assertEqual(
+            unique_texts, ["description 1", "description 2", "description 3"]
+        )
+        self.assertEqual(subheadings, ["12345678", "11111111", "22222222", "33333333"])
+        self.assertEqual(texts, [0, 1, 1, 2])
+        self.assertEqual(labels, [0, 1, 1, 3])
+
+    def test_that_it_ignores_data_with_too_few_digits(self):
+        first_data_source = StaticDataSource(
+            [("Description 1", "1111"), ("Description 2", "2222222222")],
+            creates_codes=True,
+            authoritative=False,
+        )
+
+        second_data_source = StaticDataSource(
+            [("Description 3", "33333333"), ("Description 4", "444444")],
+            creates_codes=True,
+            authoritative=False,
+        )
+
+        (unique_texts, subheadings, texts, labels) = training_data_loader.fetch_data(
+            [first_data_source, second_data_source], digits=6
+        )
+
+        self.assertEqual(
+            unique_texts, ["description 2", "description 3", "description 4"]
+        )
+        self.assertEqual(subheadings, ["222222", "333333", "444444"])
+        self.assertEqual(texts, [0, 1, 2])
+        self.assertEqual(labels, [0, 1, 2])
+
+    def test_that_it_ignores_blank_descriptions(self):
+        data_source = StaticDataSource(
+            [
+                ("Description 1", "1111111111"),
+                ("", "2222222222"),
+                ("      ", "3333333333"),
+            ],
+            creates_codes=True,
+            authoritative=False,
+        )
+
+        (unique_texts, subheadings, texts, labels) = training_data_loader.fetch_data(
+            [data_source], digits=8
+        )
+
+        self.assertEqual(unique_texts, ["description 1"])
+        self.assertEqual(subheadings, ["11111111"])
+        self.assertEqual(texts, [0])
+        self.assertEqual(labels, [0])
