9e2561e9589f3c2d1435ed029e604053738523c4
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-03-27 12:15:21 +0000

    A bit of tidying up

--- inference/infer.py
+++ inference/infer.py
@@ -9,7 +9,7 @@ score_cutoff = 0.05  # We won't send back any results with a score lower than th
 vague_term_code = "vvvvvvvvvv"
 
 transformer = os.environ.get(
-    "SENTENCE_TRANSFORMER_PRETRAINED_MODEL", "all-MiniLM-L12-v2"
+    "SENTENCE_TRANSFORMER_PRETRAINED_MODEL", "all-MiniLM-L6-v2"
 )
 transformer_cache_directory = os.environ.get(
     "SENTENCE_TRANSFORMERS_HOME", "/tmp/sentence_transformers/"
@@ -112,8 +112,8 @@ class FlatClassifier(Classifier):
 
         for i in top_results:
             # If the score is less than the cutoff then stop iterating through
-            # if i[1] < score_cutoff:
-            #     break
+            if i[1] < score_cutoff:
+                break
 
             # If we've hit the vague terms code then we'll stop iterating through
             if i[0] == vague_term_code[:digits]:
