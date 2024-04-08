531f5f918541c0afc4c0d912a2f1ac20a29e6e49
gurdipk <54589147+gurdipk@users.noreply.github.com>
2024-04-08 18:01:49 +0100

    Update infer.py
    Updated to mpnet embeddings

--- inference/infer.py
+++ inference/infer.py
@@ -9,7 +9,7 @@ score_cutoff = 0.05  # We won't send back any results with a score lower than th
 vague_term_code = "vvvvvvvvvv"
 
 transformer = os.environ.get(
-    "SENTENCE_TRANSFORMER_PRETRAINED_MODEL", "all-MiniLM-L6-v2"
+    "SENTENCE_TRANSFORMER_PRETRAINED_MODEL", "all-mpnet-base-v2"
 )
 transformer_cache_directory = os.environ.get(
     "SENTENCE_TRANSFORMERS_HOME", "/tmp/sentence_transformers/"
