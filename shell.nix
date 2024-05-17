aacde5ed2073ed486d3a7594662f48786e1dc6f7
William Fish <william.michael.fish@gmail.com>
2024-05-17 12:40:34 +0100

    FPO-201: Use underscores for documentation file names

--- shell.nix
+++ shell.nix
@@ -2,7 +2,6 @@ let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
 in pkgs.mkShell {
-  SHELL = "${pkgs.fish}/bin/fish";
   LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib/";
   buildInputs = [
     (pkgs.python311.withPackages (ps: [
