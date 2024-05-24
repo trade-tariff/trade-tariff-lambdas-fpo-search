9a44c9cde7588f93b16b21bfd28a891dbfe03bae
gurdipk <54589147+gurdipk@users.noreply.github.com>
2024-05-24 09:58:34 +0100

    Update basic_csv.py
    Remove other languages if min distance is at least 0.7 for English or None

--- data_sources/basic_csv.py
+++ data_sources/basic_csv.py
@@ -3,7 +3,9 @@ from os import PathLike
 import re
 from typing import Union
 from data_sources.data_source import DataSource
-
+from lingua import Language, LanguageDetectorBuilder
+languages = [Language.ENGLISH, Language.FRENCH, Language.GERMAN, Language.SPANISH]
+detector = LanguageDetectorBuilder.from_languages(*languages).with_minimum_relative_distance(0.7).build()
 
 class BasicCSVDataSource(DataSource):
     def __init__(
@@ -65,6 +67,44 @@ class BasicCSVDataSource(DataSource):
                 continue
             if re.search(r'^[0-9,]+$', description): # Skip rows where description contains only numbers and commas
                 continue
+
+            specific_phrase=["pendant", "moccasins", "bandanas", "it equipment", "bacillus subtilis flagellin", "voltage stabilizer", "fabric"
+                    , "primer", "lens"] #fabric vague?
+            similar_words=["cannula", "cannula injection", "naproxen", "vial", "diffuser", "carburettor", "(iso)"," iso", "antigen", "dress", " ssd", "oil"
+                ,"watch", "adapter", "hdmi", "cable", "kit", "diagnostic", "protein", "bacteria", "diagno", "rna", "allergen", "peptide"
+                ,"test", "charger", "inverter", "laptop", "ipad", "tablet", "controller", "android", "samsung", "lenovo", "thinkpad"
+                ,"primocin", "amino", "liquid", "notebook", "lingerie", "globulin", "influenza", "blood", "human", "animal", "nissan","phone"
+                , "pure", "acid", "yl", "phos", "pyr", "thymidine", "nucleic", "oxide", "benz", "toshiba", "nokia", "chloro", "oxy", "hex"
+                ,"kaftan", "chemise", "tracolimus", "bra", "footmuff", "magnifier", "photoframe", "kinder bueno", "kinder chocolate" 
+                 , "kinderbox", "katjes kinder", "mens bag", "mens shirt", "mens vest", "mens mini bag", "mens polo", "mens cologne"
+                 , "mens parka", "mens shirt", "mens linen", "mens clothing", "vest mens", "mens vest", "mens pant"
+                 , "mens bomber", "mens tshirt", "mens trainer", "mens shoes", "mens belt", "mens cap", "mens hat"
+                 , "mens denim", "mens hat", "mens tie", "mens anorak", "mens boot", "eau de parfum"]
+
+            if authoritative=False:
+                language = detector.detect_language_of(description)
+                if description in specific_phrase:
+                    # Check if any specified keywords are present in lowercase description
+                    if any(keyword==description for keyword in specific_phrase):
+                        description=description  # keep exact matches
+                    elif any(keyword.lower() in description for keyword in similar_words):
+                        description=description # keep if appears anywhere in string
+                    ###Condition to remove 'kinder' but keep certain mentions of it (above)
+                    elif 'kinder' in description:
+                        continue ##remove mentions of kinder even if confidence is higher than 0.2 / assigned as eng or None (except specific word combos above)
+                    elif language=='ENG' or language=='None':
+                        description=description ##else only keep if assigned as english or None
+                    else:
+                        continue ##else don't keep
+        
+
+            
+            if language == None:
+            language_min_rel_distance_point7.append((text, 'None', label, file))
+        else:
+            language_min_rel_distance_point7.append((text,language.iso_code_639_3.name, label, file))
+
+            
 
             if subheading in codes:
                 codes[subheading].add(description)
