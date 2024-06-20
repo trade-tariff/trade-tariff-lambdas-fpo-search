bb830b2931a3ff9dd2d481cebb65a3b8bf8140c5
William Fish <william.michael.fish@gmail.com>
2024-06-20 09:46:36 +0100

    FPO-50: Let sentence transformers do the work to batch the embeddings

--- training/create_embeddings.py
+++ training/create_embeddings.py
@@ -1,5 +1,4 @@
 import logging
-import math
 from sentence_transformers import SentenceTransformer
 import torch
 
@@ -14,39 +13,27 @@ class EmbeddingsProcessor:
     ) -> None:
         self._torch_device = torch_device
         self._batch_size = batch_size
-        self._sentence_transformer_model = SentenceTransformer(transformer_model).to(torch_device)
+        self._sentence_transformer_model = SentenceTransformer(transformer_model).to(
+            torch_device
+        )
         self._logger = logger
 
     def create_embeddings(self, texts: list[str]):
-        max_batch = int(math.ceil(len(texts) / self._batch_size))
-
         self._sentence_transformer_model.to(self._torch_device)
 
-        # Initialize an empty list to store the embeddings
-        sentence_embeddings = []
+        sentence_embeddings = self._sentence_transformer_model.encode(
+            texts,
+            batch_size=self._batch_size,
+            show_progress_bar=True,
+            convert_to_numpy=True,  # Avoid OOM issues for large datasets
+        )
 
-        # Process texts in batches
-        for i in range(max_batch):
-            start_index = i * self._batch_size
-            end_index = min((i + 1) * self._batch_size, len(texts))
+        sentence_embeddings = torch.from_numpy(sentence_embeddings)
 
-            batch_texts = texts[start_index:end_index]
+        if self._torch_device == "mps":
+            torch.mps.empty_cache()
 
-            # Encode the batch of texts
-            batch_embeddings = self._sentence_transformer_model.encode(
-                batch_texts,
-                batch_size=self._batch_size,
-                show_progress_bar=True,
-                convert_to_tensor=True,
-            )
-
-
-            # Append the batch embeddings to the list
-            sentence_embeddings.extend(batch_embeddings)
-
-            if self._torch_device == "mps":
-                torch.mps.empty_cache()
-            elif self._torch_device == "cuda":
-                torch.cuda.empty_cache()
+        elif self._torch_device == "cuda":
+            torch.cuda.empty_cache()
 
         return sentence_embeddings
