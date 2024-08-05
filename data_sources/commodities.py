c672a26cb0eb4dafe73e4e381a8015e28b97453c
William Fish <william.michael.fish@gmail.com>
2024-08-05 11:06:50 +0100

    FPO-371: Adds hierarchy descriptions to the commodities datasource

--- data_sources/commodities.py
+++ data_sources/commodities.py
@@ -14,6 +14,20 @@ class CommoditiesDataSource(DataSource):
     PATH = "/:service/reporting/:year/:month/:day/commodities_:service_:year_:month_:day.csv"
     DEFAULT_SERVICE = "uk"
 
+    SID = 0
+    COMMODITY_CODE = 1
+    PRODUCT_LINE_SUFFIX = 2
+    DESCRIPTION = 3
+    START_DATE = 4
+    END_DATE = 5
+    INDENTATION = 6
+    END_LINE = 7
+    CLASS = 8
+    ITEMIDPLUSPLS = 9
+    HIERARCHY = 10
+
+    TARGETED_LEVEL = "Commodity"
+
     def __init__(
         self,
         service: str = DEFAULT_SERVICE,
@@ -47,15 +61,31 @@ class CommoditiesDataSource(DataSource):
 
     def get_codes(self, digits: int) -> dict[str, set[str]]:
         commodities: dict[str, set[str]] = {}
+        hierarchical_descriptions: dict[str, str] = {}
 
         for index, row in enumerate(self._reader()):
             if index == 0:
                 continue
 
-            subheading = row[1][:digits]
-            description = row[3]
+            subheading: str = row[self.COMMODITY_CODE][:digits]
+            description: str = row[self.DESCRIPTION]
+            hierarchy: str = row[self.HIERARCHY]
+            item_id_plus_pls: str = row[self.ITEMIDPLUSPLS]
 
             result = None
+
+            if row[self.CLASS] != self.TARGETED_LEVEL:
+                hierarchical_descriptions[item_id_plus_pls] = description
+
+                continue
+            else:
+                acc = ""
+                for h_item_id_plus_plus in hierarchy.split(" "):
+                    if h_item_id_plus_plus in hierarchical_descriptions:
+                        acc += hierarchical_descriptions[h_item_id_plus_plus]
+
+                acc += description
+                description = acc
 
             if self._cleaning_pipeline:
                 result = self._cleaning_pipeline.filter(subheading, description)
